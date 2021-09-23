84041b22de32124b690cfd641aeea5a536d27a61
Kip <88916382+UrbaneD0ge@users.noreply.github.com>
2021-09-23 18:08:16 -0400

    fixed clock, minor css tweaks

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
-var today = moment();
+var today = $("#currentDay");
+var
+// update clock in real time
+function displayTime() {
+    var rightNow = moment().format("MMM Do, YYYY h:mm:ss a");
+    today.text(rightNow);
+}
 
-$("#currentDay").text(today.format("MMM Do, YYYY"));
+setInterval(displayTime, 1000);
+// fillable text area
+
+
+// save text to local storage
+
+// change colors w/ time
