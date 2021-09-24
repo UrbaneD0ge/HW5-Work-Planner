751b27bef81ae1e0b3c723da4cca1b7e834c6396
Kip <88916382+UrbaneD0ge@users.noreply.github.com>
2021-09-24 19:58:32 -0400

    save/load working

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 var today = $("#currentDay");
-var
+var textArea = $('textarea');
+var crntHr = moment().format('H');
+var saveBtn = $('saveBtn');
+
 // update clock in real time
 function displayTime() {
     var rightNow = moment().format("MMM Do, YYYY h:mm:ss a");
@@ -7,9 +10,43 @@ function displayTime() {
 }
 
 setInterval(displayTime, 1000);
-// fillable text area
+
+// Moment.js colorChanging
+// init () {
+//     if (crntHr.diff(id.value) > 0) {
+//     }
+// } 
 
 
-// save text to local storage
+// save text to local storage (with help from Zac G.)
+$('.saveBtn').on('click', saveLocal);
 
-// change colors w/ time
+function saveLocal () {
+    var input = $(this).siblings('textArea').val().trim();
+    var valueId = $(this).siblings('textArea').attr('id');
+    console.log(this);
+    localStorage.setItem(valueId, input);
+}
+
+function start() {
+    load();
+    function load() {
+        var sVal = [];
+        var sKey = Object.keys(localStorage);
+        i = sKey.length;
+
+        while (i--) {
+            var getItems = localStorage.getItem(sKey[i]);
+            sVal.unshift(getItems);
+        }
+    dsplay(sVal, sKey);
+    return sVal;
+
+    }
+}
+function dsplay (sVal, sKey) {
+    for (var i = 0; i < sKey.length; i++) {
+        $('#'+sKey[i]).val(sVal[i]);
+        }
+    }
+start();
